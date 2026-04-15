19cf2f494623b38c67d3fed63f8231f9c39ff594
Georges Sako <georgessako1@gmail.com>
2026-04-15 10:07:56 +0200

    feat: add membre creation dialog

--- src/app/pages/membres/membres.component.ts
+++ src/app/pages/membres/membres.component.ts
@@ -1,15 +1,17 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatDialog, MatDialogModule } from '@angular/material/dialog';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTableModule } from '@angular/material/table';
+import { MembreDialogComponent } from '../../components/membre-dialog/membre-dialog.component';
 import { Membre, MembreService } from '../../services/membre.service';
 
 @Component({
   selector: 'app-membres',
   standalone: true,
   imports: [CommonModule, MatTableModule,
-    MatButtonModule, MatIconModule],
+    MatButtonModule, MatIconModule, MatDialogModule],
     templateUrl: './membres.component.html',
     styleUrl: './membres.component.scss'
 })
@@ -18,7 +20,9 @@ export class MembresComponent implements OnInit {
   membres: Membre[] = [];
   displayedColumns: string[] = ['id', 'nom', 'prenom', 'email', 'telephone', 'solde', 'actif', 'actions'];
 
-  constructor( private membreService: MembreService) {}
+  constructor( private membreService: MembreService,
+    private dialog: MatDialog
+  ) {}
 
   ngOnInit(): void {
       this. loadMembres();
@@ -29,8 +33,23 @@ loadMembres(): void {
     next: (data: Membre[]) => this.membres = data,
     error: (err: any) => console.error('Erreur chargement membres', err)
   });
+
 }
 
+ouvrirDialog(): void {
+  const dialogRef = this.dialog.open(MembreDialogComponent, {
+    width: '500px'
+  });
+
+  dialogRef.afterClosed().subscribe((result: Membre) => {
+    if (result) {
+      this.membreService.creerMembre(result).subscribe({
+        next: () => this.loadMembres(),
+        error: (err: any) => console.error('Erreur création membre', err)
+      });
+    }
+  });
+}
 supprimerMembre(id: number): void {
   if (confirm('Voulez-vous vraiment supprimer ce membre ?')) {
     this.membreService.supprimerMembre(id).subscribe({
