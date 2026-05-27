cbd0da83ecce055c33ccd1e97a1797f3c78dfa55
Gs732 <georgessako1@gmail.com>
2026-05-27 17:04:24 +0200

    app.routers + app.config

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,22 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { MembresComponent } from './pages/membres/membres.component';
+import { TerrainsComponent } from './pages/terrains/terrains.component';
 import { ReservationsComponent } from './pages/reservations/reservations.component';
 import { SiteComponent } from './pages/site/site.component';
-import { TerrainsComponent } from './pages/terrains/terrains.component';
+import { LoginComponent } from './pages/login/login.component';
+import { authGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
-  { path: '', component: HomeComponent },
-  { path: 'membres', component: MembresComponent },
-  { path: 'sites', component: SiteComponent },
-  { path: 'terrains', component: TerrainsComponent },
-  { path: 'reservations', component: ReservationsComponent },
-  { path: '**', redirectTo: '' }
+  { path: 'login', component: LoginComponent },
+  { path: '', component: HomeComponent, canActivate: [authGuard] },
+  { path: 'membres', component: MembresComponent, canActivate: [authGuard] },
+  { path: 'sites', component: SiteComponent, canActivate: [authGuard] },
+  { path: 'terrains', component: TerrainsComponent, canActivate: [authGuard] },
+  {
+    path: 'reservations',
+    component: ReservationsComponent,
+    canActivate: [authGuard],
+  },
+  { path: '**', redirectTo: 'login' },
 ];
